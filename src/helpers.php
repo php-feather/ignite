8335782591e7cf60b40e3793054f5c6174f01f17
Francis Carbah <fcarbah@gmail.com>
2021-05-31 11:43:01 -0400

    moved helpers to ignite

--- src/helpers.php
+++ src/helpers.php
@@ -18,6 +18,91 @@ function base_path($relPath = '')
 }
 
 /**
+ * Autoload all files in the helpers directory
+ * @param array $exclude List of files to not autoload
+ * @param type $directory absolute path of directory files to autoload
+ * @return type
+ */
+function feather_autoload($directory, $exclude = [])
+{
+
+    if (!is_dir($directory)) {
+        require_once $directory;
+        return;
+    }
+
+    $files = scandir($directory);
+
+    $dirPath = strrpos($directory, '/') === strlen($directory) - 1 ? $directory : $directory . '/';
+
+    foreach ($files as $file) {
+        if (strpos($file, '.') === 0) {
+            continue;
+        }
+
+        if (is_dir($file)) {
+            feather_autoload($file);
+        } else if (!in_array($file, $exclude)) {
+            require_once $dirPath . $file;
+        }
+    }
+}
+
+/**
+ * Generates csrf Token
+ * @return string
+ */
+function fa_csrf_token()
+{
+    $csrf = \Feather\App\Security\Csrf::getInstance();
+    $token = $csrf->generateToken();
+    return $token;
+}
+
+/**
+ * Generates csrf Form Element
+ * @return type
+ */
+function fa_csrf_token_input()
+{
+    $id = CSRF_ID;
+    $token = fa_csrf_token();
+    return <<<TOKEN
+    <input type="hidden" name="$id" value="$token" />
+TOKEN;
+}
+
+/**
+ *
+ * @param string $encryptedText
+ * @return string
+ * @throws RuntimeException
+ */
+function fa_decrypt($encryptedText)
+{
+    $key = get_env('APP_KEY');
+    if (!$key) {
+        throw new RuntimeException('Application key not found! Please');
+    }
+    return fs_decrypt($encryptedText, hex2bin($key));
+}
+
+/**
+ *
+ * @param mixed $value
+ * @return string
+ * @throws RuntimeException
+ */
+function fa_encrypt($value)
+{
+    $key = get_env('APP_KEY');
+    if (!$key) {
+        throw new RuntimeException('Application key not found! Please');
+    }
+    return fs_encrypt($value, hex2bin($key));
+}
+
+/**
  * Loads a config file
  * @param string $relConfigFilepath
  * @return mixed
