c35e0dedf04785c25afd4559921bea0d92e7a71c
Francis Carbah <fcarbah@gmail.com>
2021-06-02 17:27:48 -0400

    added app container

--- src/App.php
+++ src/App.php
@@ -7,7 +7,6 @@ use Feather\Init\Http\Request;
 use Feather\Init\Http\Response;
 use Feather\Cache\ICache;
 use Feather\Session\Drivers\ISessionHandler;
-use Feather\Support\Container\Singleton as AppContainer;
 use Feather\Ignite\ErrorHandler\IErrorHandler;
 use Feather\Ignite\ErrorHandler\ErrorResolver;
 use Feather\View\IView;
@@ -61,7 +60,7 @@ final class App
 
     /**
      * List of objects registered in application Container
-     * @var \Feather\Support\Container\Singleton
+     * @var \Feather\Ignite\AppContainer
      */
     protected $container;
 
---
+++ src/AppContainer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Feather\Ignite;
+
+use Feather\Support\Container\Singleton;
+
+/**
+ * Description of AppContainer
+ *
+ * @author fcarbah
+ */
+class AppContainer extends Singleton
+{
+//put your code here
+}
