1a81b0514352ba0af267ee64a67ec1b49acb1ab0
Francis Carbah <fcarbah@gmail.com>
2022-01-09 17:09:02 -0500

    updated providers

--- src/Provider/AuthProvider.php
+++ src/Provider/AuthProvider.php
@@ -2,6 +2,8 @@
 
 namespace Feather\Ignite\Provider;
 
+use Feather\Ignite\Auth;
+
 /**
  * Description of AuthProvider
  *
@@ -12,11 +14,61 @@ class AuthProvider extends Provider
 
     public function register()
     {
-        $authConfig = $this->app->getConfig('auth');
+        $config = $this->app->getConfig('auth');
 
         if (!empty($authConfig)) {
 
+            $authConfig  = $config['authenticators'][$config['authenticator']];
+            $guardConfig = $config['guards'][$config['guard']];
+
+            $authenticator = $authConfig['class'];
+            unset($authConfig['class']);
+            $auth          = new $authenticator();
+
+            if (!$auth instanceof \Feather\Auth\IAuthenticator) {
+                throw new \Exception("$authenticator is not an instance of Feather\Auth\IAuthenticator");
+            }
+
+            foreach ($authConfig as $key => $data) {
+
+                if ($key === 'connection') {
+                    $data = $this->app->container('database')->get($data);
+                }
+
+                if (method_exists($auth, "set{$key}")) {
+                    call_user_func_array([$auth, "set{$key}"], [$data]);
+                }
+            }
+
+            $guard = $this->getGuard($guardConfig);
+
+            $auth->setGuard($guard);
+
+            return $auth;
         }
+
+        throw new \Exception('Auth configuration missing');
+    }
+
+    /**
+     *
+     * @param array $guardConfig
+     * @return \Feather\Auth\Guard
+     */
+    protected function getGuard(array $guardConfig)
+    {
+        $class = $guardConfig['class'];
+        unset($guardConfig['class']);
+
+        $guard = new $class();
+
+        foreach ($guardConfig as $key => $data) {
+            if (method_exists($guard, "set{$key}")) {
+                call_user_func_array([$guard, "set{$key}"], [$data]);
+            }
+        }
+
+        return $guard;
     }
 
 }
--- src/Provider/DatabaseProvider.php
+++ src/Provider/DatabaseProvider.php
@@ -23,14 +23,14 @@ class DatabaseProvider extends Provider
         $dbConfig  = $this->app->getConfig('database');
 
         foreach ($dbConfig['connections'] as $key => $config) {
-            $container->register($this->keyPrefix . $key, function() use($config) {
+            $container->register($key, function() use($config) {
                 $db = new Connection($config);
                 $db->connect();
                 return $db;
             });
 
             if ($key == $dbConfig['default']) {
-                $container->register($this->keyPrefix . 'default', function() use($config) {
+                $container->register('default', function() use($config) {
                     $db = new Connection($config);
                     $db->connect();
                     return $db;
